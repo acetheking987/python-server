11f065dd67faf2cda22aa9d50e84f2253d208c27
acetheking987 <86131366+acetheking987@users.noreply.github.com>
2022-04-16 00:14:13 +0100

    Update code.py

--- code.py
+++ code.py
@@ -1,4 +1,7 @@
 def main():
+  import json
   print("hello world!")
   print("this is an update!")
   print("this is another update!")
+  dict = {"obj" : "apple"}
+  open("output.json", "w").write(json.dumps(dict))
