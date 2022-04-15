32a45918336099ab7f5b220a1cae8822a51f9b13
acetheking987 <86131366+acetheking987@users.noreply.github.com>
2022-04-16 00:37:15 +0100

    Update code.py

--- code.py
+++ code.py
@@ -1,7 +1,16 @@
 def main():
-  import json
-  print("hello world!")
-  print("this is an update!")
-  print("this is another update!")
-  dict = {"obj" : "apple"}
-  open("output.json", "w").write(json.dumps(dict))
+    import json
+    import random
+
+    data = {"losses": 0, "wins": 0, "numbers": {1: 0, 2: 0, 3: 0, 4: 0, 5: 0, 6: 0}}
+
+    for i in range(5000):
+        random_number = random.randint(1, 6)
+        data["numbers"][random_number] += 1
+        if random_number == 6:
+            data["wins"] += 1
+        else:
+            data["losses"] += 1
+    
+    with open("output.json", "w") as f:
+        json.dumps(data, f)
