da746ffdbda744d80a05a04048f7af9cdc702904
acetheking987 <86131366+acetheking987@users.noreply.github.com>
2022-04-16 00:47:36 +0100

    Update code.py

--- code.py
+++ code.py
@@ -2,15 +2,20 @@ def main():
     import json
     import random
 
-    data = {"losses": 0, "wins": 0, "numbers": {1: 0, 2: 0, 3: 0, 4: 0, 5: 0, 6: 0}}
+    data = {"losses": 0,
+            "wins": 0,
+            "numbers": {1: 0, 2: 0, 3: 0, 4: 0, 5: 0, 6: 0},
+            "win rate": "0%"}
 
-    for i in range(50000):
+    for i in range(1000000):
         random_number = random.randint(1, 6)
         data["numbers"][random_number] += 1
         if random_number == 6:
             data["wins"] += 1
         else:
             data["losses"] += 1
+
+    data["win rate"] = str(round(data["wins"] / (data["wins"] + data["losses"]) * 100, 2)) + "%"
     
     with open("output.json", "w") as f:
         json.dump(data, f)
