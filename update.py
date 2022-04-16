46a54fb710a84549e31bd30e1ca36493075157bf
acetheking987 <86131366+acetheking987@users.noreply.github.com>
2022-04-16 23:52:28 +0100

    auto update

---
+++ update.py
@@ -0,0 +1,15 @@
+from github import Github
+from importlib import reload
+import main
+from time import sleep as sl
+
+G = Github(open("key.txt", "r").read())
+
+def update():
+    print("updating")
+    repo = G.get_repo("acetheking987/python-server")
+    content = repo.get_contents("main.py")
+    open("main.py", "wb").write(content.decoded_content)
+    sl(5)
+    reload(main)
+    main.main()
