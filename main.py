7ac4e336cc3f1108479f919f3358e60b7437a7c8
acetheking987 <acetheking987@gmail.com>
2022-04-17 03:01:40 +0100

    status updates

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from github import Github
 from importlib import reload
 from time import sleep as sl
+import psutil
 import programme
 import update
 import datetime
@@ -17,21 +18,41 @@ def update_check():
         update.update()
 
 def main():
+    cycles = 0
     while True:
+        cycles += 1
         try:
             update_check()
+
             repo = G.get_repo("acetheking987/python-server")
             content = repo.get_contents("code.py")
             open("programme.py", "wb").write(content.decoded_content)
+
             sl(5)
             reload(programme)
+        
             start = time.time()
             upload = programme.main()
             end = time.time()
+
             if upload:
                 file = f"{datetime.datetime.now().strftime('%d/%m/output %H:%M:%S')}.json"
                 repo.create_file(file, f"took {round(end - start, 3)}s to complete", json.dumps(upload))
+
             print(f"took {round(end - start, 3)}s to complete")
+
+            if cycles % 60 == 0:
+                content = repo.get_contents("stats.md")
+                stats = f"""
+
+                date: {datetime.datetime.now().strftime('%d/%m/%Y %H:%M:%S')}
+                cpu: {psutil.cpu_percent()}%
+                ram: {psutil.virtual_memory().percent}%
+                cycles: {cycles}
+
+                         """
+
+                repo.update_file("stats.md", "updated stats", stats, content.sha)
 
         except Exception as e:
             try:
