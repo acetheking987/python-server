7278e4bf204baadee46c36defd5e26603fee5be6
acetheking987 <86131366+acetheking987@users.noreply.github.com>
2022-04-20 07:49:28 +0100

    Update code.py

--- code.py
+++ code.py
@@ -1,7 +1,3 @@
 def main():
-    import datetime
-    words = open("words.txt", "r").read().split("\n")
-    date = str(datetime.datetime.strptime("19/04/22", "%d/%m/%y")).split(" ")[0]
-    for word in words:
-        if word.split("|")[0] == date:
-            return(f"{word.split('|')[1]}", "wordle.md")
+    print("waiting for code...")
+    return(None, None)
